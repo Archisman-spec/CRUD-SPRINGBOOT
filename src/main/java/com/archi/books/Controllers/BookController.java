f7ec6961d4b522ad4819c052a7cb35c4d855d1f8
Archisman-spec <archiarchisman@gmail.com>
2024-06-08 15:42:09 +0530

    "hogaya"

---
+++ src/main/java/com/archi/books/Controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.archi.books.Controllers;
+
+import com.archi.books.Entity.Book;
+import com.archi.books.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(final BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping(path = "/books/{isbn}")
+    public ResponseEntity<Book> createBook(@PathVariable final String isbn, @RequestBody final Book book) {
+        book.setIsbn(isbn);
+        bookService.create(book)
+    }
+
+}
--- src/test/java/com/archi/books/service/Impl/BookServiceImplTest.java
+++ src/test/java/com/archi/books/service/Impl/BookServiceImplTest.java
@@ -3,11 +3,13 @@ package com.archi.books.service.Impl;
 import com.archi.books.Entity.Book;
 import com.archi.books.Entity.BookEntity;
 import com.archi.books.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,6 +20,7 @@ public class BookServiceImplTest {
     @Mock
     private BookRepository bookRepository;
 
+    @Test
     public void testthatbookisSaved(){
         Book book = Book.builder()
                 .isbn("109325u0234").
@@ -32,6 +35,7 @@ public class BookServiceImplTest {
 
         when(bookRepository.save(bookie)).thenReturn(bookie);
         final Book result =underTest.create(book);
+        assertEquals( book, result);
 
     }
 
