7634d48e837ff63b8665f16377b56eb1f929c6e0
Archisman-spec <archiarchisman@gmail.com>
2024-06-08 22:14:52 +0530

    "hogaya"

--- src/main/java/com/archi/books/service/BookService.java
+++ src/main/java/com/archi/books/service/BookService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookService {
+
+    boolean isbookexists(Book book);
+
     Book create(Book book);
     Optional<Book> findById(String isbn);
     List<Book> listbooks();
